bd318721f6f8a4fff990482790d80e70da67e523
AlexanderKaraberov <alexander.karaberov@gmail.com>
2015-08-26 16:34:21 +0300

    Initial Commit

---
+++ SwiftLand/SwiftLand.h
@@ -0,0 +1,19 @@
+//
+//  SwiftLand.h
+//  SwiftLand
+//
+//  Created by Alexander on 8/26/15.
+//  Copyright (c) 2015 Alexander Karaberov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftLand.
+FOUNDATION_EXPORT double SwiftLandVersionNumber;
+
+//! Project version string for SwiftLand.
+FOUNDATION_EXPORT const unsigned char SwiftLandVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftLand/PublicHeader.h>
+
+
