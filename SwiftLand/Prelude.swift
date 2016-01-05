b920b2a1a142d72aa534db05fc9e6092dcedbc91
AlexanderKaraberov <alexander.karaberov@gmail.com>
2016-01-05 18:23:25 +0200

    Add S-combinator and psi combinators, comment fixes

--- SwiftLand/Prelude.swift
+++ SwiftLand/Prelude.swift
@@ -9,21 +9,36 @@
 import Foundation
 
 
-/// The identity function; returns its argument.
+/// The identity function; always returns its argument.
 public func id<T>(x: T) -> T {
     return x
 }
 
 
-/// Returns a function which ignores its argument and returns `x` instead.
-public func const<T, U>(x: T?) -> U -> T? {
-    return { _ in x }
+/// K- combinator : constant combinator ignores its second argument and always returns its first argument.
+public func const<A, B>(x : A) -> B -> A {
+    return { (_) in
+        return x
+    }
 }
 
 
 /// Returns a pair with its fields in the opposite order.
 public func swap<A, B>(a: A, b: B) -> (B, A) {
     return (b, a)
+}
+
+
+/// S-combinator Applies the second function to a value, then applies the first function to a value and the
+/// result of the previous function application.
+public func substitute<R, A, B>(f : R -> A -> B) -> (R -> A) -> R -> B {
+    return { g in { x in f(x)(g(x)) } }
+}
+
+/// A type-restricted version of const.  In cases of typing ambiguity, using this function forces
+/// its first argument to resolve to the type of the second argument.
+public func asTypeOf<A>(x : A) -> A -> A {
+    return const(x)
 }
 
 /// Returns the least fixed point of the function returned by `f`.
@@ -58,7 +73,7 @@ public func flip<T, U, V, W, X>(f: (T, U, V, W) -> X) -> (W, V, U, T) -> X {
     return { f($3, $2, $1, $0) }
 }
 
-/// On | Applies the function on its right to both its arguments, then applies the function on its
+/// On | P-combinator (Psi) Applies the function on its right to both its arguments, then applies the function on its
 /// left to the result of both prior applications.
 ///
 ///    (+) |*| f = { x in { y in f(x) + f(y) } }
@@ -66,7 +81,7 @@ public func on<A, B, C>(o : B -> B -> C) -> (A -> B) -> A -> A -> C {
     return { f in { x in { y in o(f(x))(f(y)) } } }
 }
 
-/// On | Applies the function on its right to both its arguments, then applies the function on its
+/// On | P-combinator (Psi) Applies the function on its right to both its arguments, then applies the function on its
 /// left to the result of both prior applications.
 ///
 ///    (+) |*| f = { x, y in f(x) + f(y) }
