a089b0c69393c734511607452d41518076943db9
Alexander Karaberov <alexander.karaberov@gmail.com>
2017-01-10 12:49:56 +0200

    Remove old, irrelevant code

--- SwiftLand/Utilities.swift
+++ SwiftLand/Utilities.swift
@@ -9,30 +9,6 @@
 import Foundation
 
 
-///Method to validate input email address
-public func isValidEmail(_ email: String) -> Bool {
-    
-    let emailRegExp: String = "^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}" +
-    "[a-zA-Z0-9])?(?:\\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$"
-    
-    let emailTestPredicate = NSPredicate(format:"SELF MATCHES %@", emailRegExp)
-    return emailTestPredicate.evaluate(with: email)
-}
-
-///This function is an analogue of the handy macro NSDictionaryOfVariableBindings from Objective-C
-///but instead of view names you should use ordinal numeration: view1, view2... viewN.
-public func dictionaryOfVariableBindings(_ variables: UIView...) -> Dictionary<String, UIView> {
-    
-    var dictionary = Dictionary<String, UIView>()
-    
-    for (index, element) in variables.enumerated() {
-        dictionary["view\(index + 1)"] = element
-    }
-    
-    return dictionary
-}
-
-
 /// Applies a function to an argument until a given predicate returns true.
 public func until<A>(_ p : @escaping (A) -> Bool) -> ( @escaping (A) -> A) -> (A) -> A {
     return { f in { x in p(x) ? x : until(p)(f)(f(x)) } }
